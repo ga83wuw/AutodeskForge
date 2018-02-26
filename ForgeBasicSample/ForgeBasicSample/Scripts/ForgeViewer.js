61c06b034b262251384b0d08252b0145f7603d27
George <georgiospathanasiou@yahoo.com>
2018-02-26 20:07:19 +0200

    Done

---
+++ ForgeBasicSample/ForgeBasicSample/Scripts/ForgeViewer.js
@@ -0,0 +1,73 @@
+﻿var viewer;
+
+function showModel(urn) {
+
+    var options = {
+        env: 'AutodeskProduction',
+        getAccessToken: getAccessToken,
+        refreshToken: getAccessToken
+    };
+    var documentId = 'urn:' + urn;
+    Autodesk.Viewing.Initializer(options, function onInitialized() {
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+    });
+}
+
+function getAccessToken() {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", '/api/forge/token', false /* forge viewer requires SYNC*/);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+/**
+* Autodesk.Viewing.Document.load() success callback.
+* Proceeds with model initialization.
+*/
+function onDocumentLoadSuccess(doc) {
+
+    // A document contains references to 3D and 2D viewables.
+    var viewables = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type':'geometry'}, true);
+    if (viewables.length === 0) {
+        console.error('Document contains no viewables.');
+        return;
+    }
+
+    // Choose any of the avialble viewables
+    var initialViewable = viewables[0];
+    var svfUrl = doc.getViewablePath(initialViewable);
+    var modelOptions = {
+        sharedPropertyDbPath: doc.getPropertyDbPath()
+    };
+
+    var viewerDiv = document.getElementById('forgeViewer');
+    viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
+    viewer.start(svfUrl, modelOptions, onLoadModelSuccess, onLoadModelError);
+}
+
+/**
+ * Autodesk.Viewing.Document.load() failuire callback.
+ */
+function onDocumentLoadFailure(viewerErrorCode) {
+    console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
+}
+
+/**
+ * viewer.loadModel() success callback.
+ * Invoked after the model's SVF has been initially loaded.
+ * It may trigger before any geometry has been downloaded and displayed on-screen.
+ */
+function onLoadModelSuccess(model) {
+    console.log('onLoadModelSuccess()!');
+    console.log('Validate model loaded: ' + (viewer.model === model));
+    console.log(model);
+}
+
+/**
+ * viewer.loadModel() failure callback.
+ * Invoked when there's an error fetching the SVF file.
+ */
+function onLoadModelError(viewerErrorCode) {
+    console.error('onLoadModelError() - errorCode:' + viewerErrorCode);
+}
---
+++ ForgeBasicSample/ForgeBasicSample/default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" Async="true" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ForgeBasicSample._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Forge Viewer</title>
+    <link rel="stylesheet" href="https://developer.api.autodesk.com/modelderivative/v2/viewers/style.min.css" type="text/css" />
+    <script src="https://developer.api.autodesk.com/modelderivative/v2/viewers/three.min.js"></script>
+    <script src="https://developer.api.autodesk.com/modelderivative/v2/viewers/viewer3D.min.js"></script>
+    <script src="/Scripts/ForgeViewer.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload &amp; Translate" />
+            <div id="forgeViewer">
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
